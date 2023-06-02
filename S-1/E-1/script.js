1b3c6c78b93f9c2f8449f2be1f7ba3ff7f4ed006
Subham Sahu <about.subhamsahu@gmail.com>
2023-06-02 22:40:35 +0530

    Section-1 :: Status => Finished

---
+++ S-1/E-1/script.js
@@ -0,0 +1,15 @@
+/*
+// select the body element
+const body = document.body;
+
+// Manipulate the body element
+body.style.backgroundColor = '#C4DFDF';
+body.innerHTML = '<h1>Hello, world!</h1>';
+*/
+
+// creating heading
+const heading = document.createElement("h1");
+heading.innerHTML = `Hello World from JavaScript!`;
+
+const root = document.getElementById('root');
+root.appendChild(heading);  
