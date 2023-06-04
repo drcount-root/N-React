fd19a14bcb7f2145b3e0027895c1b8a06e8c1cb2
Subham Sahu <about.subhamsahu@gmail.com>
2023-06-04 10:08:32 +0530

    .

---
+++ S-1/E-3/navbar/src/components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Logo() {
+  return (
+    <React.Fragment>
+        <div>
+            <h1 className='text-3xl'>Food Incernate</h1>
+        </div>
+    </React.Fragment>
+  )
+}
